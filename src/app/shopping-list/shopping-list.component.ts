8f02822644d9ed8469aeb0dd33cf580129a9a704
Kirill <chuyanov.k@gmail.com>
2018-07-14 19:52:11 +0300

    update to angular 6

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,7 +1,7 @@
 import { Ingredient } from './../shared/ingredient.model';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ShoppingListService } from './shopping-list.service';
-import { Subscription } from 'rxjs/Subscription';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
