ecf9dbdd3f3c9d4b8a1c8f421fb2f8092ba3d980
Kirill <chuyanov.k@gmail.com>
2018-08-11 15:30:39 +0300

    optimize preloading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { HomeComponent } from './core/home/home.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
@@ -11,7 +11,9 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes)],
+    imports: [
+        RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules })
+    ],
     exports: [RouterModule]
 })
 export class AppRoutingModule {}
